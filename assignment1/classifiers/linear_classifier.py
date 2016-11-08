09882025065a825b4d21288d451bb6c5307caa13
HanZhipeng <hanzhipeng@pku.edu.cn>
2016-11-08 18:42:25 +0800

    HZP: delete cs231n
    Signed-off-by: HanZhipeng <hanzhipeng@pku.edu.cn>
    
    The directory for the classifier are changed, so the code regarding need
    to be changed.

--- assignment1/classifiers/linear_classifier.py
+++ assignment1/classifiers/linear_classifier.py
@@ -1,6 +1,6 @@
 import numpy as np
-from cs231n.classifiers.linear_svm import *
-from cs231n.classifiers.softmax import *
+from classifiers.linear_svm import *
+from classifiers.softmax import *
 
 class LinearClassifier(object):
 
