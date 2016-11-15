c149e6cdcaad6a353c9803965d78427aa040175e
HanZhipeng <hanzhipeng@pku.edu.cn>
2016-11-15 18:09:05 +0800

    HZP: implement svm loss function and linear classifier train and predict.
    Signed-off-by: HanZhipeng <hanzhipeng@pku.edu.cn>
    
    The linear_classifier.py contains both of svm and softmax classifiers.I
    have successed in implementing train and predict functions(note that: the
    doc for the input in the predict function is contrary.)
    In this commit, the loss function for svm is implemented.

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -49,6 +49,10 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
+      # choice the batch and get the index of the samples
+      choiceIndex = np.random.choice(num_train, batch_size, replace = False)
+      X_batch = X[choiceIndex]
+      y_batch = y[choiceIndex]
       pass
       #########################################################################
       #                       END OF YOUR CODE                                #
@@ -63,6 +67,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
+      self.W -= learning_rate * grad
       pass
       #########################################################################
       #                       END OF YOUR CODE                                #
@@ -79,7 +84,7 @@ class LinearClassifier(object):
     data points.
 
     Inputs:
-    - X: D x N array of training data. Each column is a D-dimensional point.
+    - X: N x D array of training data. Each row is a D-dimensional point.
 
     Returns:
     - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
@@ -91,6 +96,9 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
+    # Scores: N * C
+    Scores = X.dot(self.W)
+    y_pred = np.argmax(Scores, axis = 1)
     pass
     ###########################################################################
     #                           END OF YOUR CODE                              #
--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -26,7 +26,7 @@ def svm_loss_naive(W, X, y, reg):
   num_train = X.shape[0]
   loss = 0.0
   for i in xrange(num_train):
-    scores = X[i].dot(W)
+    scores = X[i].dot(W)    # scores: a numpy array of shape (1, C)
     correct_class_score = scores[y[i]]
     for j in xrange(num_classes):
       if j == y[i]:
@@ -34,14 +34,22 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        # calculate the dW,
+        dW[:, j] += X[i, :].T
+        dW[:, y[i]] -= X[i, :].T
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
-
+  dW /= num_train
+  
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
-
+  # we need to add reg * W :
+  # L = 1/N * Li + 0.5 * reg * W**2
+  # do partial derivatives on this formula
+  dW += reg * W
+  
   #############################################################################
   # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dW.                #
@@ -50,7 +58,7 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
+  
 
   return loss, dW
 
@@ -60,6 +68,16 @@ def svm_loss_vectorized(W, X, y, reg):
   Structured SVM loss function, vectorized implementation.
 
   Inputs and outputs are the same as svm_loss_naive.
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C. (N,) means 1 * N
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
   """
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
@@ -69,6 +87,20 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
+  num_train = X.shape[0]
+  Scores = X.dot(W) # Scores: N * C
+  # correct_class_scores: 1 * N
+  correct_class_scores = Scores[np.arange(num_train), y]
+  # correct_class_scores.T equals to itself.
+  # so we need to transpose Scores to statify the dimension. and Margins is C * N
+  Margins = np.maximum(0, Scores.T - correct_class_scores + 1) # delta set to 1
+  Margins = Margins.T # Margins: N * C
+  Margins[np.arange(num_train), y] = 0
+          
+  loss = np.sum(Margins)
+  loss /= num_train
+  # add regularization
+  loss += 0.5 * reg * np.sum(W*W)
   pass
   #############################################################################
   #                             END OF YOUR CODE                              #
@@ -84,6 +116,20 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
+  Binary = Margins
+  # Binary[i][j] = 1 means: for i-th sample, Sj - Syi + 1 > 0
+  Binary[Margins > 0] = 1
+
+  # (1) for all j: dW[j, :] sum_{i, j produces positive margin with i} X[:, i].T
+  # (2) for all i: dW[y[i], :] sum_{y != y_i, j produces positive margin with i} -X
+  # col_sum respond to how much x_i need to sub.
+  col_sum = np.sum(Binary, axis = 1)
+  Binary[np.arange(num_train), y] = -col_sum[xrange(num_train)]
+  dW = np.dot(X.T, Binary)
+  # mean
+  dW /= num_train
+  # Regularize
+  dW += reg * W
   pass
   #############################################################################
   #                             END OF YOUR CODE                              #
